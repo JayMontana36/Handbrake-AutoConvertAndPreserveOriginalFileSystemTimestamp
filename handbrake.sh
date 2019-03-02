b83cf4ccd35a1490260e3816fb238e0c28366066
Jasen Samuels <jasensamuels@gmail.com>
2019-03-01 21:08:54 -0500

    Update handbrake.sh

--- handbrake.sh
+++ handbrake.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 #
+HOME=/data/mediaserver/Handbrake
 TOUCH=touch
-SRC=/data/mediaserver/Handbrake/RAW/
-DEST=/data/mediaserver/Handbrake/Post/
+SRC=$HOME/RAW
+DEST=$HOME/Post
 DEST_EXT=mkv
 HANDBRAKE_CLI=HandBrakeCLI
 PRESET="H.265 MKV 1080p30"
 IFS=$(echo -en "\n\b")
 #
+if [ -f "$HOME/Handbrake.running" ]; then
+        exit
+fi
+#
+$TOUCH $HOME/Handbrake.running
 #
 for FILE in `ls $SRC`
 do
@@ -16,6 +22,11 @@ do
         filename=${filename%.*}
 #
         $HANDBRAKE_CLI --all-audio --all-subtitles -i "$SRC/$FILE" -o "$DEST/$filename.$DEST_EXT" --preset="$PRESET"
-        $TOUCH -r "$SRC/$FILE" "$DEST/$filename.$DEST_EXT"
-        #rm -rf "$SRC/$FILE"
+        if [ -f "$DEST/$filename.$DEST_EXT" ]; then
+                $TOUCH -r "$SRC/$FILE" "$DEST/$filename.$DEST_EXT"
+                #mv "$SRC/$FILE" "$DEST/RAW/$FILE"
+                #rm -rf "$SRC/$FILE"
+                fi
 done
+#
+rm -rf $HOME/Handbrake.running
